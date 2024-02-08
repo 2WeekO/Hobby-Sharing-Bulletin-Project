369568a2d0cd0999865691fb14f647564807a5e3
2WeekO <boom568@naver.com>
2024-02-08 22:15:06 +0900

    Please enter the commit message for your changes. Lines starting

--- db.js
+++ db.js
@@ -1,11 +1,25 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// 
+// // MongoDB 연결
+// mongoose.connect('mongodb://localhost:27017/board_db', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('MongoDB에 연결되었습니다.'))
+// .catch(err => console.error('MongoDB 연결 중 오류 발생:', err));
+// 
+// module.exports = mongoose;
 
-// MongoDB 연결
-mongoose.connect('mongodb://localhost:27017/board_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB에 연결되었습니다.'))
-.catch(err => console.error('MongoDB 연결 중 오류 발생:', err));
+var mysql = require('mysql');
 
-module.exports = mongoose;
+var db = mysql.createConnection({
+  host : '호스트주소',
+  user : '유저',
+  password : '비밀번호',
+  database : 'DB',
+  dataStrings: 'date',
+
+});
+db.connect();
+
+module.exports = db;
